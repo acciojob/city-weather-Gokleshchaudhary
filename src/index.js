ec89ed1de560ddfc0411275a2c1a892387feeefd
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-04 11:36:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import 'regenerator-runtime/runtime'; // ✅ Required for async/await
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
